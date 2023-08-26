fc6caec02c08297ed27290a10f0205cf438675ad
jspark2000 <idpjs2000@gmail.com>
2023-08-26 11:18:12 +0000

    fix: notice page text indent and font size

--- src/views/notice/noticeView.vue
+++ src/views/notice/noticeView.vue
@@ -12,121 +12,231 @@
     <div class="mt-6 border-t border-gray-100">
       <dl class="divide-y divide-gray-100">
         <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-0">
-          <dt class="text-sm font-semibold leading-6 text-gray-900">대회명</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+          <dt class="text-sm font-semibold leading-6 text-gray-900">1. 대회명</dt>
+          <dd class="mt-1 text-xs sm:text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
             2023 홍천군체육회장배 서울경기인천강원 추계 대학미식축구 선수권전
           </dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-0">
-          <dt class="text-sm font-semibold leading-6 text-gray-900">장소</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+          <dt class="text-sm font-semibold leading-6 text-gray-900">2. 장소</dt>
+          <dd class="mt-1 text-xs sm:text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
             홍천 종합운동장
           </dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-0">
-          <dt class="text-sm font-semibold leading-6 text-gray-900">주관 및 주최</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+          <dt class="text-sm font-semibold leading-6 text-gray-900">3. 주관 및 주최</dt>
+          <dd class="mt-1 text-xs sm:text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
             서울 미식축구협회, 경기강원 미식축구협회
           </dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-0">
-          <dt class="text-sm font-semibold leading-6 text-gray-900">후원</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+          <dt class="text-sm font-semibold leading-6 text-gray-900">4. 후원</dt>
+          <dd class="mt-1 text-xs sm:text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
             홍천군, 홍천군체육회, 대한미식축구협회, PlayProve
           </dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-0">
-          <dt class="text-sm font-semibold leading-6 text-gray-900">대회 운영</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
-            가. 참가 자격 <br />
-            2023년 8월 기준 서울미식축구협회 등록 대학교 미식축구팀,경기강원 협회 등록 대학교 <br />
-            · 14팀 ( 건국대학교, 고려대학교, 국민대학교, 동국대학교, 서강대학교, 서울대학교,
-            서울시립대학교, 성균관대학교, 숭실대학교, 연세대학교, 중앙대학교, 한국외국어대학교,
-            한양대학교, 홍익대학교 ) <br />
-            · 6팀 ( 단국대학교,용인대학교,강원대학교,인하대학교,한림대학교,한신대학교 ) <br />
-            · 반드시 대한미식축구협회에 등록된 팀명으로 신청 <br />
-            · 경희대학교는 ‘23년 하반기 창단 예정 <br /><br />
-            나. 참가 신청 마감 : 2023년 8월 25일 (출전비를 마감일까지 납부한 팀에 한해 참가 신청
-            유효) <br /><br />
-            다. 출전비 <br />
-            · 1부 팀당 1,500,000원 납부 <br />
-            · 2부 팀당 1,250,000원 납부 <br />
-            · 특별찬조금 1부 1위 ~ 4위 팀당 500,000원 납부 (2023년 9월 23일까지) (계좌번호
-            3333-27-0767612 카카오뱅크 서울미식축구협회) <br /><br />
-            라. 시상 <br />
-            · 팀 시상 : 1위, 2위, 3위 <br />
-            - 서울,경기강원미식축구협회 등록 팀/선수들의 페어플레이 도모<br />
-            위해, 대회 종료 전 각 팀 감독/주장의 투표를 통해 페어플레이 팀 선정 <br />
-            · 개인 시상 : MVP, 최우수 공격상, 최우수 수비상, 매니저상, 지도자상, 심판상 <br />
-            - 투표 방식은 추후 공지 <br /><br />
-            마. 신청 서류 <br />
-            참가신청서 (양식1), 서약서 (양식 2), 등록 선수 명단 (양식 3), 신분증 및 재적 관련 증명서
-            (재학/휴학/졸업) 사본을 2023년 8월 31일 20시까지 구글폼 링크로 제출
+          <dt class="text-sm font-semibold leading-6 text-gray-900">5. 대회 운영</dt>
+          <dd class="mt-1 text-xs sm:text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+            <p>가. 참가 자격</p>
+            <ul class="list-disc ml-4">
+              <li>2023년 8월 기준 서울 미식축구협회 또는 경기강원 미식축구협회 등록 대학교</li>
+              <li>
+                14팀 (건국대학교, 고려대학교, 국민대학교, 동국대학교, 서강대학교, 서울대학교,
+                서울시립대학교, 성균관대학교, 숭실대학교, 연세대학교, 중앙대학교, 한국외국어대학교,
+                한양대학교, 홍익대학교)
+              </li>
+              <li>6팀 (단국대학교, 용인대학교, 강원대학교, 인하대학교, 한림대학교, 한신대학교)</li>
+              <li>반드시 대한미식축구협회에 등록된 팀명으로 신청</li>
+              <li>경희대학교는 ‘23년 하반기 창단 예정</li>
+            </ul>
             <br />
-            · 등록선수명단은 추계대회 해당 대학의 첫 시합 전일까지 수정 가능하나, 선수 추가 등록 시
-            재적 관련 증명서를 첨부하여야 함. 추가 등록하는 선수의 재적 관련 증명서 미확인 시
-            부정선수로 간주함. <br /><br />
-            바. 선수 자격 <br />
-            · 참가 신청 대학교에 재적 중인 대학생 및 대학원생(대학원생 참가 자격은 대학원 입학일
-            기준으로 2년 이내) <br />
-            - 국내 교류학생의 경우 대회 참가 불가하며, 외국인 교환학생의 경우 등록 학기 내 최소
-            3학점 이상 수강 중인 정규 수강생으로 등록자격을 제한함. <br /><br />
-            사. 경기자격 확인 <br />
-            · 경기 출전선수 명단 <br />
-            - 경기 당일 기 제출한 선수명단과 출전 선수를 대면 확인함. <br />
-            - 명단에 없는 선수가 경기 당일 추가되는 경우는 선수 자격이 없음. <br /><br />
-            아. 운동장 출입 및 장비, 복장 규정 <br />
-            · 미식축구 공식 규칙 제 1편 (경기, 경기장, 선수와 장비) 제 4장 (선수와 장비)에 적합한
-            장비 및 복장 <br />
-            - 헤드기어 색 및 상-하의 색깔 통일되어야 함. <br />
-            - 선수의 건강, 안전 확인을 위해 유색 Visor 착용을 엄격히 금함. <br />
-            · 팀 박스 출입 : 참가 등록 선수, 코치 및 스태프에 한함 <br />
-            - 사전 출전선수 명단 제출시 코칭스태프 명단 및 스태프 인원 명단에 포함되어 있어야 함.
-            <br />
-            - 각 팀 OB를 포함한 모든 경기 관람객은 팀 박스가 아닌 관객석에 위치해야 하며, 경기 당일
-            코치/스태프 추가 등록 불가함. <br />
-            · 코치 및 스태프 복장 규정 : 슬리퍼, 샌들, 아쿠아슈즈 및 민소매 착용 시 해당인원 퇴장 및
-            팀 경고 부여 <br />
-            - 대회 기간 동안 총 3회 팀 경고 받은 팀의 경우 몰수패 처리 예정 <br />
-            - 단, 장비 및 복장의 규정은 규칙 관리 범주에 포함되어, 적용 및 처분의 권한은 심판협회에
-            위임함.
 
-            <br /><br />
-            자. 경기 및 심판 운영 <br />
-            · 1부와 2부로 나누어 진행 <br />
-            · 전년도 추계대회 성적 기준으로 1부팀과 2부팀 결정 <br />
-            · 경기 당 7심 (필드 6심+ 게시 1심)을 기본으로 운영 <br />
-            · 심판 운영 <br />
-            - 팀당 2명씩의 졸업생을 의무심판으로 배정함. <br />
-            - 모든 의무 심판은 모교 경기 심판 배정 불가하며, 평가 공정성을 위해 매경기 최소 3개 이상
-            학교 출신의 심판진을 운영하는 것을 원칙으로 함. <br />
-            - 단, 졸업생 수가 절대적으로 부족한 팀 창립 6년 미만 신생팀의 경우 경기보조 회수 및 인원
-            확대로 대체함. <br />
-            - 대회 종료시까지 2인 이상의 졸업생 의무 심판을 규정대로 운영하지 못하는 팀의 경우,
-            추계대회 종료 직후에 “전체 추계대회 경기 수 x 1만원 x 7심“의 심판 지원비를 심판협회에
-            지불해야 함. (ex. 10팀 참가 – 15 경기 운영 시 총 105만원 추가 납부) <br />
-            - 심판 지원비 미납부 팀은 완납할 때까지 2024년 대회 참가 불가함. <br />
-            - 심판 판정에 대한 어필 권한은 각 팀 감독, 주장, 오펜스/디펜스 캡틴에게 한정적으로
-            부여함. 권한을 갖고 있지 않은 인원의 어필 시 1차 경고, 2차 경기 퇴장 조치함. <br />
-            - 각 팀 감독은 사이드라인 밖에서 심판 판정에 대한 어필을 진행해야 하며, 허가 받지 않은
-            경기장 출입 시 즉시 퇴장될 수 있음. <br />
-            · 서울협회 경기운영위원장은 심판이 욕설, 폭행, 명백한 오심 등 심판의 권위에 해를 끼치는
-            행위를 하였다고 판단하는 경우, 해당 심판에 대해 1. 잔여경기 출장 정지, 2. 벌금 납부, 3.
-            심판 자격 정지 등의 징계를 심판협회에 요청할 수 있다. 징계요청을 받은 심판협회는
-            의무적으로 징계위원회를 개최하여야 하며, 동 징계위원회에는 서울협회 경기운영위원장과
-            사무국장이 참석함. <br /><br />
-            차. 경기 보조 <br />
-            · 경기 당 2개 학교 5명씩 동원해 총 10명으로 경기보조 실시<br />
-            · 경기 전 : 경기장 및 본부석 세팅 (전체 인원) <br />
-            · 경기 중: 다운판 & 체인 (3명), 볼 배급 (4명), 계시 및 기록 (3명) <br />
-            · 경기 후: 경기장 및 본부석 정리, 경기장 내 전체 쓰레기 정리 (전체 인원) <br />
-            · 경기 진행에 방해가 되는 행위, 무단 해산, 서울협회 지시사항에 따르지 않을 시 경기보조
-            지원금 미지급 및 경기보조 1회 추가 배치
+            <p>나. 참가 신청 마감</p>
+            <ul class="list-disc ml-4">
+              <li>2023년 8월 31일 (출전비를 마감일까지 납부한 팀에 한해 참가 신청 유효)</li>
+            </ul>
+            <br />
+
+            <p>다. 출전비</p>
+            <ul class="list-disc ml-4">
+              <li>1부 팀당 1,500,000원 납부</li>
+              <li>2부 팀당 1,250,000원 납부</li>
+              <li>
+                특별찬조금 1부 1위 ~ 4위 팀당 500,000원 납부
+                <ul class="list-disc pl-4">
+                  <li>2023년 9월 23일까지</li>
+                  <li>계좌번호 3333-27-0767612 카카오뱅크 서울미식축구협회</li>
+                </ul>
+              </li>
+            </ul>
+            <br />
+
+            <p>라. 시상</p>
+            <ul class="list-disc ml-4">
+              <li>
+                팀 시상 : 1위, 2위, 3위
+                <ul class="list-disc pl-4">
+                  <li>
+                    서울,경기강원 미식축구협회 등록 팀/선수들의 페어플레이 도모 위해, 대회 종료 전
+                    각 팀 감독/주장의 투표를 통해 페어플레이 팀 선정
+                  </li>
+                </ul>
+              </li>
+              <li>
+                개인 시상 : MVP, 최우수 공격상, 최우수 수비상, 매니저상, 지도자상, 심판상
+                <ul class="list-disc pl-4">
+                  <li>투표 방식은 추후 공지</li>
+                </ul>
+              </li>
+            </ul>
+            <br />
+
+            <p>마. 신청 서류</p>
+            <ul class="list-disc ml-4">
+              <li>
+                아래에 나열된 서류들을 2023년 8월 31일 20시까지 구글폼 링크로 제출
+                <ul class="list-disc pl-4">
+                  <li>출전 신청서</li>
+                  <li>출전 서약서</li>
+                  <li>출전 선수 명단</li>
+                  <li>신분증 및 재적 관련 증명서 (재학/휴학/졸업) 사본</li>
+                </ul>
+              </li>
+              <li>서류 양식은 [7. 첨부서류]에서 다운로드 받을 수 있다.</li>
+              <li>
+                등록선수명단은 추계대회 해당 대학의 첫 시합 전일까지 수정 가능하나, 선수 추가 등록
+                시 재적 관련 증명서를 첨부하여야 함. 추가 등록하는 선수의 재적 관련 증명서 미확인 시
+                부정선수로 간주함.
+              </li>
+            </ul>
+            <br />
+
+            <p>바. 선수 자격</p>
+            <ul class="list-disc ml-4">
+              <li>
+                참가 신청 대학교에 재적 중인 대학생 및 대학원생(대학원생 참가 자격은 대학원 입학일
+                기준으로 2년 이내)
+              </li>
+              <li>
+                국내 교류학생의 경우 대회 참가 불가하며, 외국인 교환학생의 경우 등록 학기 내 최소
+                3학점 이상 수강 중인 정규 수강생으로 등록자격을 제한함
+              </li>
+            </ul>
+            <br />
+
+            <p>사. 경기자격 확인</p>
+            <ul class="list-disc ml-4">
+              <li>
+                경기 출전선수 명단
+                <ul class="list-disc pl-4">
+                  <li>경기 당일 기 제출한 선수명단과 출전 선수를 대면 확인함.</li>
+                  <li>명단에 없는 선수가 경기 당일 추가되는 경우는 선수 자격이 없음.</li>
+                </ul>
+              </li>
+            </ul>
+            <br />
+
+            <p>아. 운동장 출입 및 장비, 복장 규정</p>
+            <ul class="list-disc ml-4">
+              <li>
+                미식축구 공식 규칙 제 1편 (경기, 경기장, 선수와 장비) 제 4장 (선수와 장비)에 적합한
+                장비 및 복장
+                <ul class="list-disc pl-4">
+                  <li>헤드기어 색 및 상-하의 색깔 통일되어야 함.</li>
+                  <li>선수의 건강, 안전 확인을 위해 유색 Visor 착용을 엄격히 금함.</li>
+                </ul>
+              </li>
+              <li>
+                팀 박스 출입 : 참가 등록 선수, 코치 및 스태프에 한함
+                <ul class="list-disc pl-4">
+                  <li>
+                    사전 출전선수 명단 제출시 코칭스태프 명단 및 스태프 인원 명단에 포함되어 있어야
+                    함.
+                  </li>
+                  <li>
+                    각 팀 OB를 포함한 모든 경기 관람객은 팀 박스가 아닌 관객석에 위치해야 하며, 경기
+                    당일 코치/스태프 추가 등록 불가함.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                코치 및 스태프 복장 규정 : 슬리퍼, 샌들, 아쿠아슈즈 및 민소매 착용 시 해당인원 퇴장
+                및 팀 경고 부여
+                <ul class="list-disc pl-4">
+                  <li>대회 기간 동안 총 3회 팀 경고 받은 팀의 경우 몰수패 처리 예정</li>
+                  <li>
+                    단, 장비 및 복장의 규정은 규칙 관리 범주에 포함되어, 적용 및 처분의 권한은
+                    심판협회에 위임함.
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <br />
+            <p>자. 경기 및 심판 운영</p>
+            <ul class="list-disc ml-4">
+              <li>1부와 2부로 나누어 진행</li>
+              <li>전년도 추계대회 성적 기준으로 1부팀과 2부팀 결정</li>
+              <li>경기 당 7심 (필드 6심+ 게시 1심)을 기본으로 운영</li>
+              <li>
+                심판 운영
+                <ul class="list-disc pl-4">
+                  <li>팀당 2명씩의 졸업생을 의무심판으로 배정함.</li>
+                  <li>
+                    모든 의무 심판은 모교 경기 심판 배정 불가하며, 평가 공정성을 위해 매경기 최소
+                    3개 이상 학교 출신의 심판진을 운영하는 것을 원칙으로 함.
+                  </li>
+                  <li>
+                    대회 종료시까지 2인 이상의 졸업생 의무 심판을 규정대로 운영하지 못하는 팀의
+                    경우, 추계대회 종료 직후에 “전체 추계대회 경기 수 x 1만원 x 7심“의 심판 지원비를
+                    심판협회에 지불해야 함.
+                    <ul class="list-disc pl-4">
+                      <li>ex. 10팀 참가 –> 15 경기 운영 시 총 105만원 추가 납부</li>
+                    </ul>
+                  </li>
+                  <li>심판 지원비 미납부 팀은 완납할 때까지 2024년 대회 참가 불가함.</li>
+                  <li>
+                    심판 판정에 대한 어필 권한은 각 팀 감독, 주장, 오펜스/디펜스 캡틴에게 한정적으로
+                    부여함. 권한을 갖고 있지 않은 인원의 어필 시 1차 경고, 2차 경기 퇴장 조치함.
+                  </li>
+                  <li>
+                    각 팀 감독은 사이드라인 밖에서 심판 판정에 대한 어필을 진행해야 하며, 허가 받지
+                    않은 경기장 출입 시 즉시 퇴장될 수 있음.
+                  </li>
+                </ul>
+              </li>
+              <li>
+                서울협회 경기운영위원장은 심판이 욕설, 폭행, 명백한 오심 등 심판의 권위에 해를
+                끼치는 행위를 하였다고 판단하는 경우, 해당 심판에 대해 아래와 같은 징계들을
+                심판협회에 요청할 수 있다.
+                <ul class="list-disc pl-4">
+                  <li>잔여경기 출장 정지</li>
+                  <li>벌금 납부</li>
+                  <li>심판 자격 정지</li>
+                </ul>
+              </li>
+              <li>
+                징계요청을 받은 심판협회는 의무적으로 징계위원회를 개최하여야 하며, 동
+                징계위원회에는 서울협회 경기운영위원장과 사무국장이 참석함.
+              </li>
+            </ul>
+            <br />
+            <p>차. 경기 보조</p>
+            <ul class="list-disc ml-4">
+              <li>경기 당 2개 학교 5명씩 동원해 총 10명으로 경기보조 실시</li>
+              <li>경기 전: 경기장 및 본부석 세팅 (전체 인원)</li>
+              <li>경기 중: 다운판 & 체인 (3명), 볼 배급 (4명), 계시 및 기록 (3명)</li>
+              <li>경기 후: 경기장 및 본부석 정리, 경기장 내 전체 쓰레기 정리 (전체 인원)</li>
+              <li>
+                경기 진행에 방해가 되는 행위, 무단 해산, 서울협회 지시사항에 따르지 않을 시 경기보조
+                지원금 미지급 및 경기보조 1회 추가 배치
+              </li>
+            </ul>
           </dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-0">
-          <dt class="text-sm font-semibold leading-6 text-gray-900">공지사항</dt>
-          <dd class="mt-1 text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
+          <dt class="text-sm font-semibold leading-6 text-gray-900">6. 공지사항</dt>
+          <dd class="mt-1 text-xs sm:text-sm leading-6 text-gray-700 sm:col-span-3 sm:mt-0">
             가. 경기장 대관 일정에 따라 대회 기간 및 세부 일정은 일부 조정될 수 있음 <br /><br />
             나. 본 대회는 미식축구 공식규칙 제 1편 (경기, 경기장, 선수와 장비) 제 3장 (볼)에 적합한
             해당 팀 고유의 공을 사용함. <br /><br />
@@ -155,14 +265,16 @@
           </dd>
         </div>
         <div class="px-4 py-6 sm:grid sm:grid-cols-4 sm:gap-4 sm:px-0">
-          <dt class="text-sm font-semibold leading-6 text-gray-900">첨부 서류</dt>
+          <dt class="text-sm font-semibold leading-6 text-gray-900">7. 첨부 서류</dt>
           <dd class="mt-2 text-sm text-gray-900 sm:col-span-3 sm:mt-0">
             <ul role="list" class="divide-y divide-gray-100 rounded-md border border-gray-200">
-              <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+              <li
+                class="flex items-center justify-between py-4 pl-4 pr-5 text-xs sm:text-sm leading-6"
+              >
                 <div class="flex w-0 flex-1 items-center">
                   <PaperClipIcon class="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
                   <div class="ml-4 flex min-w-0 flex-1 gap-2">
-                    <span class="truncate font-medium">대회 요강</span>
+                    <span class="truncate font-medium">대회 요강.pdf</span>
                     <span class="flex-shrink-0 text-gray-400">122kb</span>
                   </div>
                 </div>
@@ -171,15 +283,17 @@
                     href="/docs/대회요강.pdf"
                     class="font-medium text-teal-500 hover:text-teal-400"
                     target="_blank"
-                    >Download</a
+                    >다운로드</a
                   >
                 </div>
               </li>
-              <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+              <li
+                class="flex items-center justify-between py-4 pl-4 pr-5 text-xs sm:text-sm leading-6"
+              >
                 <div class="flex w-0 flex-1 items-center">
                   <PaperClipIcon class="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
                   <div class="ml-4 flex min-w-0 flex-1 gap-2">
-                    <span class="truncate font-medium">출전 선수 명단</span>
+                    <span class="truncate font-medium">출전 선수 명단.xlsx</span>
                     <span class="flex-shrink-0 text-gray-400">13.3kb</span>
                   </div>
                 </div>
@@ -188,15 +302,17 @@
                     href="/docs/출전선수명단.xlsx"
                     class="font-medium text-teal-500 hover:text-teal-400"
                     target="_blank"
-                    >Download</a
+                    >다운로드</a
                   >
                 </div>
               </li>
-              <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+              <li
+                class="flex items-center justify-between py-4 pl-4 pr-5 text-xs sm:text-sm leading-6"
+              >
                 <div class="flex w-0 flex-1 items-center">
                   <PaperClipIcon class="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
                   <div class="ml-4 flex min-w-0 flex-1 gap-2">
-                    <span class="truncate font-medium">출전 서약서</span>
+                    <span class="truncate font-medium">출전 서약서.docx</span>
                     <span class="flex-shrink-0 text-gray-400">144kb</span>
                   </div>
                 </div>
@@ -205,15 +321,17 @@
                     href="/docs/출전서약서.docx"
                     class="font-medium text-teal-500 hover:text-teal-400"
                     target="_blank"
-                    >Download</a
+                    >다운로드</a
                   >
                 </div>
               </li>
-              <li class="flex items-center justify-between py-4 pl-4 pr-5 text-sm leading-6">
+              <li
+                class="flex items-center justify-between py-4 pl-4 pr-5 text-xs sm:text-sm leading-6"
+              >
                 <div class="flex w-0 flex-1 items-center">
                   <PaperClipIcon class="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
                   <div class="ml-4 flex min-w-0 flex-1 gap-2">
-                    <span class="truncate font-medium">출전 신청서</span>
+                    <span class="truncate font-medium">출전 신청서.docx</span>
                     <span class="flex-shrink-0 text-gray-400">143kb</span>
                   </div>
                 </div>
@@ -222,7 +340,7 @@
                     href="/docs/출전신청서.docx"
                     class="font-medium text-teal-500 hover:text-teal-400"
                     target="_blank"
-                    >Download</a
+                    >다운로드</a
                   >
                 </div>
               </li>
