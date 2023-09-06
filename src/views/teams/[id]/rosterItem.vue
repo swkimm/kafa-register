ebaed2311bb422e0c5d31078fef769410845ae94
swkimm <106427696+swkimm@users.noreply.github.com>
2023-09-06 21:22:06 +0900

    feature: roster-img-insert 완료 (#46)

---
+++ src/views/teams/[id]/rosterItem.vue
@@ -0,0 +1,61 @@
+<template>
+  <br />
+  <!-- <h1
+    v-if="roster?.name === null || roster?.name === undefined"
+    class="mb-5 font-extrabold font-mono text-2xl text-left"
+  >
+    팀명 Roster
+  </h1>
+  <h1 v-else class="mb-5 font-extrabold font-mono text-2xl text-left">
+    {{ getRemainingWords(roster?.name) }} Roster
+  </h1> -->
+
+  <tr class="border-b-2">
+    <td class="text-xs pl-2">
+      <img :src="member?.profileImgUrl" class="h-48 w-32 sm:h-30 sm:w-15 mr-1" />
+    </td>
+    <td class="text-xs sm:text-base pl-2 py-3">{{ member?.name }}</td>
+    <td class="text-xs sm:text-base pl-2 py-3">{{ member?.backNumber }}</td>
+    <td class="hidden sm:table-cell pl-2 py-3">{{ member?.height }}</td>
+    <td class="hidden sm:table-cell pl-2 py-3">{{ member?.weight }}</td>
+    <td class="text-xs sm:text-base pl-2 py-3">
+      {{ member?.position[0] }} {{ member?.position[1] }}
+    </td>
+    <td class="hidden sm:table-cell text-xs sm:text-base pl-2 py-3">
+      {{ formatRegistDate(member?.registrationDate) }}
+    </td>
+  </tr>
+</template>
+<script lang="ts" setup>
+import { type PropType, toRefs } from 'vue'
+
+interface TeamMember {
+  id: number
+  teamId: number
+  name: string
+  backNumber: number
+  registrationDate: string
+  type: string
+  weight: number
+  height: number
+  position: [string, string]
+  profileImgUrl: string
+}
+
+const props = defineProps({
+  member: {
+    type: Object as PropType<TeamMember>,
+    required: true
+  }
+})
+
+const formatRegistDate = (dateString: string): number => {
+  const date = new Date(dateString)
+  const year = date.getFullYear()
+  return year
+}
+
+const { member } = toRefs(props)
+</script>
+
+<style scoped></style>
--- src/views/teams/[id]/teamDetailView.vue
+++ src/views/teams/[id]/teamDetailView.vue
@@ -1,28 +1,52 @@
 <template>
-  <div class="max-w-screen-xl px-5 mx-auto bg-cover bg-center text-black w-full h-full">
+  <div class="max-w-screen-xl mx-auto bg-cover bg-center text-black w-full h-full">
     <TeamBannerItem v-if="teamDetail" :banner="teamDetail" />
     <br />
     <div class="items-center text-center">
-      <button class="mr-10" @click="toggleInfo">Info</button>
-      <button class="mr-10" @click="toggleRoaster">Roaster</button>
-      <button class="mr-10">Stat</button>
+      <button class="mr-10 text-center" @click="toggleInfo">Info</button>
+      <button class="mr-10" @click="toggleRoster">Roster</button>
+      <button class="mr-10">Stats</button>
       <br />
     </div>
 
-    <div class="mx-10 sm:mx-20">
+    <div class="mx-10 items-center text-center">
       <div v-if="infoVisible && teamDetail">
         <ContentItem v-if="teamDetail" :content="teamDetail" />
         <br />
         <IntroItem v-if="teamDetail" :intro="teamDetail" />
       </div>
       <br />
-      <RoasterItem v-if="roasterVisible && teamDetail" :roaster="teamDetail" />
+      <div v-if="rosterVisible && teamDetail">
+        <table
+          class="p-10 ax-auto w-full table-auto bg-white rounded-2xl border-black overflow-hidden"
+        >
+          <thead class="text-left">
+            <tr class="border-b-2">
+              <th class="text-xs sm:text-base pl-2 py-2"></th>
+              <th class="text-xs sm:text-base pl-2 py-2">Name</th>
+              <th class="text-xs sm:text-base pl-2 py-2">Number</th>
+              <th class="hidden sm:table-cell text-xs sm:text-base pl-2 py-2">Height</th>
+              <th class="hidden sm:table-cell text-xs sm:text-base pl-2 py-2">Weight</th>
+              <th class="text-xs sm:text-base pl-2 py-2">Position</th>
+              <th class="hidden sm:table-cell text-xs sm:text-base pl-2 py-2">Exprience</th>
+            </tr>
+          </thead>
+          <tbody>
+            <RosterItem
+              v-for="(member, index) in teamMember"
+              :key="member.id"
+              :member="member"
+              :class="index % 2 === 0 ? 'bg-white' : 'bg-[#F7F7F4]'"
+            />
+          </tbody>
+        </table>
+      </div>
       <br />
     </div>
   </div>
 </template>
 <script lang="ts" setup>
-import RoasterItem from './roasterItem.vue'
+import RosterItem from './rosterItem.vue'
 import ContentItem from './contentItem.vue'
 import TeamBannerItem from './teamBannerItem.vue'
 
@@ -47,20 +71,36 @@ interface TeamDetail {
     name: string
   }
 }
-const infoVisible = ref(true)
-const roasterVisible = ref(false)
 
-const toggleRoaster = () => {
+interface TeamMember {
+  id: number
+  teamId: number
+  name: string
+  backNumber: number
+  registrationDate: string
+  type: string
+  weight: number
+  height: number
+  position: [string, string]
+  profileImgUrl: string
+}
+
+const infoVisible = ref(true)
+const rosterVisible = ref(false)
+
+const toggleRoster = () => {
   infoVisible.value = false
-  roasterVisible.value = true
+  rosterVisible.value = true
+  getTeamMember()
 }
 
 const toggleInfo = () => {
-  roasterVisible.value = false
+  rosterVisible.value = false
   infoVisible.value = true
 }
 
 const teamDetail = ref<TeamDetail | null | undefined>()
+const teamMember = ref<TeamMember[] | null | undefined>()
 
 const teamId = useRoute().params.id
 
@@ -77,8 +117,23 @@ const getTeamDetail = async () => {
     })
 }
 
+const getTeamMember = async () => {
+  axiosInstance
+    .get(`/team/${teamId}/members`)
+    .then((response) => {
+      teamMember.value = response.data
+      console.log(response.data)
+    })
+    .catch((error) => {
+      if (error) {
+        console.error('선수 프로필 불러오기 오류')
+      }
+    })
+}
+
 onMounted(async () => {
   await getTeamDetail()
+  await getTeamMember()
 })
 </script>
 
