cad859a43063c23eb20cb35de0a2037aaeef035f
jspark2000 <idpjs2000@gmail.com>
2023-09-13 01:38:13 +0000

    fix: modify typo

--- src/views/teams/interface/team.interface.ts
+++ src/views/teams/interface/team.interface.ts
@@ -3,7 +3,7 @@ export interface Team {
   workout_id: number
   name: string
   message?: string
-  profile_img_id: string
+  profile_img_url: string
   created_at: string
   association_id: number
   initial: string
