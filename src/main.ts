ceb0d04c7d00150caa238bbaf529e756bc0befa8
Jisoo Park <102132161+jspark2000@users.noreply.github.com>
2023-10-06 13:33:10 +0900

    fix: add google analytics plugin (#90)

--- src/main.ts
+++ src/main.ts
@@ -13,6 +13,7 @@ import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 import { faCaretLeft, faCaretRight } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { createHead } from '@vueuse/head'
+import VueGtag from 'vue-gtag'
 
 const app = createApp(App)
 const head = createHead()
@@ -26,6 +27,20 @@ loadFonts()
 
 // configure pinia
 app.use(createPinia())
+
+// configure GA
+app.use(
+  VueGtag,
+  {
+    config: {
+      id: 'G-0K8R406LPX',
+      params: {
+        send_page_view: false
+      }
+    }
+  },
+  router
+)
 
 // configure router
 app.use(router)
